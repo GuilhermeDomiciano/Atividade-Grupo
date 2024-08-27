7ab92dc3223b9a385d4b2e804358ee0f0ad73e09
Domiciano <guilhermedomicianosilva@gmail.com>
2024-08-26 21:11:27 -0300

    um codigo python ai que eu fiz

---
+++ main.py
@@ -0,0 +1,10 @@
+while True:
+    print("fala galera to so testando aqui")
+
+    num1 = input("digita qualquer numero:")
+    num2 = input("mais um:")
+    print(num1+num2)
+
+    sair = input("s - sair:")
+    if sair.lower() == "s":
+        break
